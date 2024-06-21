dd9ede7cbcc08c04dbaf5558dfffe1da0d84e5b0
Andre <andrevitorlopes@gmail.com>
2024-06-21 13:03:34 +0200

    Add simple stuff

---
+++ main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {}
+
+resource "aws_sqs_queue" "queue" {
+  name = "test-simple-queue"
+}
